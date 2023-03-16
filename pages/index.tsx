82c75479162e83b75f7bf6bba48a4c6a88ce9dda
Nik Danilov <nik@leaflets.io>
2023-03-16 17:12:29 +0100

    Latest changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,26 +11,19 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Chat GPT TestApp</title>
+        <title>Easy Job Description</title>
         <meta name="description" content="Chat GPT TestApp" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={'bg-white min-h-screen ' + roboto.className}>
-        <div className="flex flex-col items-center justify-center px-4 py-2">
+        <div className="flex flex-col items-center justify-center px-4 py-8">
           <h1 className="text-4xl md:text-6xl font-bold">
-            Chat GPT TestApp
+            Easy Job Description
             <span className="text-4xl md:text-6xl font-bold text-blue-600">
               .
             </span>
           </h1>
-          <p className="mt-3 text-2xl">
-            Create Beautiful
-            <span className="text-2xl font-bold text-blue-600">
-              {''} Job Descriptions {''}
-            </span>
-            in Seconds
-          </p>
         </div>
         <Dashboard />
       </main>
